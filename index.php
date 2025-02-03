cf7c914c2d2c91bf0e7da2b7aa22bd26d8df2c7e
GhollamallahYoussef <youssef.ghollamallah@laplateforme.io>
2025-02-03 10:31:55 +0100

    début de la création du cv avec la partie contact

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="je vous présente mon curriculum vitae.">
+    <title>Youssef Ghollamallah - CV </title>
+</head>
+<body>
+    <aside>
+        <article>
+            <h2>Contact</h2>
+        </article>
+    </aside>
+    <main></main>
+</body>
+</html>
