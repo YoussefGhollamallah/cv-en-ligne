a6350248874d33ddb4b3a4b50232df5ec3ef1d4f
GhollamallahYoussef <youssef.ghollamallah@laplateforme.io>
2025-02-04 10:35:01 +0100

    ajout d'élément de contact et début d'ajout des compétentes

--- index.php
+++ index.php
@@ -4,12 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="je vous présente mon curriculum vitae.">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Youssef Ghollamallah - CV </title>
 </head>
 <body>
     <aside>
         <article>
-            <h2>Contact</h2>
+            <h2>CONTACT</h2>
+            <hr>
+            <ul>
+                <li><a href="tel:+33750847599">07.50.84.75.99</a></li>
+                <li><a target="_blank" href="mailto:youssef.gh@live.fr">youssef.gh@live.fr</a></li>
+                <li><a target="_blank" href="http://www.linkedin.com/in/youssef-ghollamallah">linkedin</a></li>
+                <li><a href="http://github.com/YoussefGhollamallah" target="_blank" rel="noopener noreferrer">github</a></li>
+            </ul>
+        </article>
+        <article>
+            <h2>SKILLS</h2>
+            <hr>
         </article>
     </aside>
     <main></main>
