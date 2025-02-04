d9edf670af6e9a1292085ba81a097a3080b4882d
GhollamallahYoussef <youssef.ghollamallah@laplateforme.io>
2025-02-04 14:46:41 +0100

    ajout des compétences et modification de l'onglet contact

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="je vous présente mon curriculum vitae.">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css"<?php echo time(); ?>>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>Youssef Ghollamallah - CV </title>
 </head>
 <body>
@@ -12,18 +13,34 @@
         <article>
             <h2>CONTACT</h2>
             <hr>
-            <ul>
-                <li><a href="tel:+33750847599">07.50.84.75.99</a></li>
-                <li><a target="_blank" href="mailto:youssef.gh@live.fr">youssef.gh@live.fr</a></li>
-                <li><a target="_blank" href="http://www.linkedin.com/in/youssef-ghollamallah">linkedin</a></li>
-                <li><a href="http://github.com/YoussefGhollamallah" target="_blank" rel="noopener noreferrer">github</a></li>
+            <ul class="lists">
+                <li><i class="fas fa-phone"></i> <a href="tel:+33750847599">07.50.84.75.99</a></li>
+                <li><i class="fas fa-envelope"></i> <a target="_blank" href="mailto:youssef.gh@live.fr">youssef.gh@live.fr</a></li>
+                <li><i class="fab fa-linkedin"></i> <a target="_blank" href="http://www.linkedin.com/in/youssef-ghollamallah">linkedin</a></li>
+                <li><i class="fab fa-github"></i> <a href="http://github.com/YoussefGhollamallah" target="_blank" rel="noopener noreferrer">github</a></li>
             </ul>
         </article>
+        <br>
         <article>
-            <h2>SKILLS</h2>
+            <h2>Compétences</h2>
             <hr>
+            <ul class="lists">
+                <li><strong> FRONT-END</strong> : HTML - CSS - Javascript</li>
+                <li><strong> BACK-END </strong> : PHP - NodeJS</li>
+                <li><strong> FRAMEWORK </strong> : ReactJS - SYMFONY - ExpressJS</li>
+                <li><strong> BDD </strong> : MongoDB - MySQL</li>
+                <li>Programmation Orientée Objet (POO)</li>
+                <li>Développement d'API</li>
+            </ul>
+        </article>
+        <br>
+        <article>
+            <h2>Soft skill</h2>
+            <hr>
+
         </article>
     </aside>
     <main></main>
+    <script src="assets/js/main.js"></script>
 </body>
 </html>
